c63694fb68eba5f06d8628c23ce66a37a2f2427e
eduardoranucci <eduardoranucci@outlook.com>
2024-04-25 19:07:03 -0300

    inputs js

---
+++ main.js
@@ -0,0 +1,11 @@
+const inputNome = document.getElementById('input_nome')
+const inputRa = document.getElementById('input_ra')
+const inputEmail = document.getElementById('input_email')
+const inputProva1 = document.getElementById('input_prova_1')
+const inputProvaIntegrada1 = document.getElementById('input_prova_integrada_1')
+const inputAep1 = document.getElementById('input_aep_1')
+const inputProva2 = document.getElementById('input_prova_2')
+const inputProvaIntegrada2 = document.getElementById('input_prova_integrada_2')
+const inputAep2 = document.getElementById('input_aep_2')
+
+let listaAlunos = []

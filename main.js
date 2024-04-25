f8a3ae7400400d9abbfcbefda7bf443db3bac877
eduardoranucci <eduardoranucci@outlook.com>
2024-04-25 20:11:45 -0300

    function adicionaDadosAluno

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ const inputProvaIntegrada2 = document.getElementById('input_prova_integrada_2')
 const inputAep2 = document.getElementById('input_aep_2')
 
 let listaAlunos = []
+
+function adicionaDadosAluno() {
+
+
+    listaAlunos.push({
+        nome: inputNome.value,
+        email: inputEmail.value,
+        ra: inputRa.value,
+        notas: { 
+            primeiroBimestre: {
+                prova: inputProva1.value,
+                integrada: inputProvaIntegrada1.value,
+                aep: inputAep1.value,
+            }, 
+            segundoBimestre: {
+                prova: inputProva2.value,
+                integrada: inputProvaIntegrada2.value,
+                aep: inputAep2.value,
+            }
+        }
+    })
+}
